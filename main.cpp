aefedd126a58a39de7cbac7d4bb0dbd84f216cd8
sofiaip <peraltasofiai10@gmail.com>
2021-09-07 23:28:51 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "mbed.h"
 #include <stdlib.h>
-#include <stdio.h>
 
+/**
+ * @brief Union de estructura que contiene un mapa de bit
+ * se usa para banderas
+ * 
+ */
 typedef union{
     struct{
         unsigned char b0: 1;
@@ -16,33 +20,125 @@ typedef union{
     unsigned char byte;
 }_flag;
 
+/**
+ * @brief Declaro mi flag
+ * 
+ */
 _flag flag1;
 
+/**
+ * @brief Defino valor encendido-apagado
+ * 
+ */
 #define ON          1
 #define OFF         0
 
+/**
+ * @brief Defino valor para cuando una sentencia es verdadera o falsa
+ * 
+ */
 #define TRUE        1
 #define FALSE       0
 
+/**
+ * @brief Defino el intervalo entre lecturas para "filtrar" el ruido del Botón
+ * 
+ */
 #define INTERVAL    40
 
+/**
+ * @brief Defino el intervalo entre latidos del hearbeat
+ * 
+ */
 #define MS          200
-#define TPRESS      1000    
+/**
+ * @brief Defino el intervalo de espera al presionar una tecla
+ * 
+ */
+#define TPRESS      1000 
+/**
+ * @brief Defino minimo valor del tiempo aleatorio
+ * 
+ */   
 #define BASETIME    500
+/**
+ * @brief Defino maximo valor del tiempo aleatorio
+ * 
+ */
 #define TIMEMAX     1501
+/**
+ * @brief Defino el numero de leds
+ * 
+ */
 #define MAXLED      4
+/**
+ * @brief Defino el numero de botones
+ * 
+ */
 #define NUMBOTONES  4
+/**
+ * @brief Defino el intervalo entre latidos del hearbeat
+ * 
+ */
 
+/**
+ * @brief defino los valores de la maquina de estado
+ * 
+ */
 #define ESPERAR         0
 #define JUEGO           1
 #define JUEGOTERMINADO  2
 #define TECLAS          3
 
-#define RIGHTPRESS      flag1.bit.b1
-#define WON             flag1.bit.b2
+/**
+ * @brief Defino el intervalo de destello al finalizar el juego
+ * 
+ */
+#define FINISHED        500
+/**
+ * @brief Defino cantidad de destellos +1
+ * Comienza con led en 0 
+ * Enciende 3 veces
+ * Apaga 3 veces
+ */
+#define DESTELLOS       7
+/**
+ * @brief Defino valor de todos los leds
+ * 
+ */
+#define TOTALLED        15
 
-
-
+/**
+ * @brief Bandera para identificar si fue presionado el boton correcto
+ * 1 = boton correcto
+ * 0 = boton incorrecto
+ */
+#define RIGHTPRESS      flag1.bit.b0
+/**
+ * @brief Bandera para identificar si se gano el juego
+ * 1 = verdadero
+ * 0 = falso
+ */
+#define WON             flag1.bit.b1
+/**
+ * @brief Bandera para identificar si se empezo el juego
+ * 1 = verdadero
+ * 0 = falso
+ */
+#define GAMEON          flag1.bit.b2 
+/**
+* @brief Bandera para identificar el estado del led
+ * 1 = encendido
+ * 0 = apagado
+ */
+#define STATE           flag1.bit.b3    
+   
+/**
+ * @brief Enumeración que contiene los estados de la máquina de estados(MEF) que se implementa para 
+ * el "debounce" 
+ * El botón está configurado como PullUp, establece un valor lógico 1 cuando no se presiona
+ * y cambia a valor lógico 0 cuando es presionado.
+ */
 typedef enum{
     BUTTON_DOWN,//0
     BUTTON_UP,//1
@@ -50,6 +146,11 @@ typedef enum{
     BUTTON_RISING//3
 }_eButtonState;
 
+/**
+ * @brief Estructura para reconocer el estado de un boton
+ * para conocer su tiempo presionado
+ * y la diferencia de tiempo
+ */
 typedef struct{
     uint8_t estado;
     int32_t timeDown;
@@ -57,47 +158,88 @@ typedef struct{
 }_sTeclas;
 
 
-
+/**
+ * @brief Declaro array de tipo _sTeclas del tamanio de la cantidad de botones 
+ * 
+ */
 _sTeclas ourButton[NUMBOTONES];
 
-uint16_t mask[] = {0x0001,0x0002,0x0004,0x0008};
+/**
+ * @brief Dato del tipo uint16_t para uso de mascara
+ * 
+ */
+uint16_t mask[] = {0x0001,0x0002,0x0004,0x0008,0xF};
 
+/**
+ * @brief Dato del tipo uint8_t para controlar la maquina de estados
+ * 
+ */
 uint8_t estadoJuego=ESPERAR;
 
+
+/**
+ * @brief Dato del tipo Enum para asignar los estados de la MEF
+ * 
+ */
 _eButtonState myButton;
 
+/**
+ * @brief Máquina de Estados Finitos(MEF)
+ * Actualiza el estado del botón cada vez que se invoca.
+ * 
+ * @param buttonState Este parámetro le pasa a la MEF el estado del botón.
+ */
 void actuallizaMef(uint8_t indice); 
 
+/**
+ * @brief Inicializa la MEF
+ * Le dá un estado inicial a myButton
+ */
 void startMef(uint8_t indice);
 
-void togleLed(uint8_t indice);
+/**
+ * @brief Función para cambiar el estado del LED cada vez que sea llamada.
+ * 
+ */
+void togleLed(uint8_t indice,uint8_t state);
 
+/**
+ * @brief Identifico las entradas asignada a los botones
+ * 
+ */
 BusIn botones(PB_6,PB_7,PB_8,PB_9);
+/**
+ * @brief Identifico las salidas asignada a los leds
+ * 
+ */
 BusOut leds(PB_12,PB_13,PB_14,PB_15);
 
+/**
+ * @brief defino la salida para el Hearbeat(led)
+ * 
+ */
 DigitalOut HearBeat(PC_13);
 
+/**
+ * @brief variable donde voy a almacenar el tiempo del timmer una vez cumplido
+ * 
+ */
 Timer miTimer;
 
 
-
-//int tiempoSB = 0;
-
 int tiempoMs = 0;
-
-RIGHTPRESS=0;
-WON=0;
-
-
 
 int main(){
     miTimer.start();
-    
-    int HearBeat=0;
+
+    WON=0;
+    GAMEON=0;
+    RIGHTPRESS=0;
+    STATE=0;
 
     int tiempoBeat=0;
-
-    tiempoBeat = miTimer.read_ms();
+    int tiempoF=0;
+    int cont=0;
     
     uint16_t ledAuxRandom=0;
     
@@ -110,7 +252,7 @@ int main(){
     int tiempoRandom=0;
     
     int ledAuxJuegoStart=0;
-    uint8_t cont=0;
+    
     
     for(uint8_t indice=0; indice<NUMBOTONES;indice++){
       startMef(indice);  
@@ -127,7 +269,9 @@ int main(){
                             if(ourButton[indice].timeDiff >= TPRESS){
                                 srand(miTimer.read_us());
                                 estadoJuego = TECLAS;
-                     
+                                for(uint8_t indice=0; indice<NUMBOTONES;indice++ ){
+                                    startMef(indice);
+                                }
                             } 
                         }
                     }
@@ -138,47 +282,59 @@ int main(){
                         for(indiceAux=0; indiceAux<NUMBOTONES; indiceAux++){
                             actuallizaMef(indiceAux);
                             if(ourButton[indiceAux].estado!=BUTTON_UP)
-                            break;
+                                break;
                         }
                         if(indiceAux==NUMBOTONES){
-                            estadoJuego = JUEGO;
-                            leds=15;
+                            leds=TOTALLED;
                             ledAuxJuegoStart = miTimer.read_ms();
+                            estadoJuego = JUEGO;
+                            for(uint8_t indice=0; indice<NUMBOTONES;indice++ ){//
+                                    startMef(indice);
+                            }
+                            
                         }
                         
                         
             break;
             case JUEGO:
                 if(leds==0){
-                    ledAuxRandom = rand() % (MAXLED);
-                    togleLed(ledAuxRandom);
-                    ledAuxRandomTime = (rand() % (TIMEMAX))+BASETIME;
-                    tiempoRandom=miTimer.read_ms();
-                    leds=mask[ledAuxRandom];
-                }else{
                     if((miTimer.read_ms()-ledAuxJuegoStart)>TPRESS){
-                        if(leds==15){
+                        GAMEON=1;
+                    }
+                    if(GAMEON){
+                        ledAuxRandom = rand() % (MAXLED);
+                        togleLed(ledAuxRandom,ON);
+                        ledAuxRandomTime = (rand() % (TIMEMAX))+BASETIME;
+                        tiempoRandom=miTimer.read_ms();
+                        leds=mask[ledAuxRandom];
+                    }
+                }else{
+                    if(leds==15){
+                        if((miTimer.read_ms()-ledAuxJuegoStart)>TPRESS){
                             ledAuxJuegoStart=miTimer.read_ms();
                             leds=0;
                         }
                     }
                 }
                 
-                if ((miTimer.read_ms()-tiempoRandom)<=ledAuxRandomTime){
-                    for(indicePress = 0; indicePress<NUMBOTONES;indicePress++){
-                            actuallizaMef(indicePress);
-                            if(ourButton[indicePress].estado == BUTTON_DOWN){
-                                cont++;
-                                if(indicePress == ledAuxRandom){
-                                    RIGHTPRESS=1;
+               
+                for(indicePress = 0; indicePress<NUMBOTONES;indicePress++){
+                                actuallizaMef(indicePress);
+                                if(ourButton[indicePress].estado == BUTTON_DOWN){
+                                    if(indicePress == ledAuxRandom){
+                                        RIGHTPRESS=1;
+                                    }
+                                    if(indicePress != ledAuxRandom){
+                                        RIGHTPRESS=0;
+                                        break;
+                                    }
                                 }
-                            }
-                    }
-                    
                 }
-                if ((miTimer.read_ms()-tiempoRandom)>ledAuxRandomTime){
+                  
+                
+                if((miTimer.read_ms()-tiempoRandom)>ledAuxRandomTime && GAMEON){
                     leds=0;
-                    if(cont==1 && RIGHTPRESS==1){
+                    if(RIGHTPRESS==1){
                         WON = 1;
                     }else{
                         WON=0;
@@ -189,10 +345,35 @@ int main(){
             break;
             case JUEGOTERMINADO:
                 if(WON){
-
+                    if((miTimer.read_ms()-tiempoF)>FINISHED){
+                        togleLed(MAXLED,STATE);
+                        tiempoF=miTimer.read_ms();
+                        STATE=!STATE;
+                        cont++;
+                    }
+                    
+                    
                 }else{
-
-                }
+                    if((miTimer.read_ms()-tiempoF)>FINISHED){
+                            togleLed(ledAuxRandom,STATE);
+                            tiempoF=miTimer.read_ms();
+                            STATE=!STATE;
+                            cont++;
+                        }
+                    }
+                    if(cont==DESTELLOS){
+                        for(uint8_t indice=0; indice<NUMBOTONES;indice++){
+                            startMef(indice); 
+                            ourButton[indice].timeDiff = 0;
+                            ourButton[indice].timeDown=0;
+                        }
+                        tiempoF = miTimer.read_ms();
+                        cont=0;
+                        leds=0;
+                        GAMEON=0;
+                        estadoJuego=ESPERAR;
+                    }
+                
             break;
             default :
                     estadoJuego = ESPERAR;
@@ -204,16 +385,14 @@ int main(){
            HearBeat = !HearBeat;
 
        }
-       if((miTimer.read_ms()-tiempoRandom) > ledAuxRandomTime){
-           leds=0;
-       }
 
       
        
 
 
-    return 0;
+    
     }
+    return 0;
 }
 
 void startMef(uint8_t indice){  
@@ -231,12 +410,12 @@ void actuallizaMef(uint8_t indice){
             }
         break;
         case BUTTON_UP:
-            if(!botones.read() & mask[indice])
+            if(!(botones.read() & mask[indice]))
                 ourButton[indice].estado = BUTTON_FALLING;
         
         break;
         case BUTTON_FALLING:
-            if(!botones.read() & mask[indice]){
+            if(!(botones.read() & mask[indice])){
                 ourButton[indice].estado = BUTTON_DOWN;
                 //Flanco de bajada
                 ourButton[indice].timeDown = miTimer.read_ms();
@@ -262,6 +441,10 @@ void actuallizaMef(uint8_t indice){
     }
 }
 
-void togleLed(uint8_t indice){
-    leds = mask[indice];
+void togleLed(uint8_t indice,uint8_t state){
+    if(state){
+        leds = mask[indice];
+    }else{
+        leds=0;
+    }
 }
